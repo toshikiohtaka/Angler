6529c4adf20b6b76f67dc67593e2ece78d9fe25f
大高俊輝 <to.so.lo.1205@gmail.com>
2017-07-06 16:38:27 +0900

    edit production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,9 @@ Rails.application.configure do
 
   config.action_controller.perform_caching = true
 
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  config.server_static_assets = true
 
   config.assets.js_compressor = :uglifier
 
